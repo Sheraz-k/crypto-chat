7558dc5bdd2a95a2382222f21821e9f5df8495e6
Sheraz <sheraznflp@gmail.com>
2022-08-18 15:52:13 +0500

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -38,7 +38,7 @@ const App = () => {
   return (
     <div className={classes.wrapper}>
       <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography variant="h2" align="center">Video Chat</Typography>
+        <Typography variant="h2" align="center">Cryptoverse Chat</Typography>
       </AppBar>
       <VideoPlayer />
       <Sidebar>
